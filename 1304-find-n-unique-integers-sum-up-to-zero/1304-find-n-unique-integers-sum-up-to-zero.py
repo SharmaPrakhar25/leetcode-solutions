9fef1d0e6942ec3956ab6092ca7357c7b174eb5a
Prakhar Sharma <prakhar.sharma2512@gmail.com>
2022-01-11 21:46:03 +0530

    Time: 60 ms (9.98%), Space: 14.1 MB (91.47%) - LeetHub

--- 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.py
+++ 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.py
@@ -3,11 +3,17 @@ class Solution:
         ans = [0] * n
         num = n//2
         idx = 0 
-        while num > 0:
-            ans[idx] = num
-            ans[n-idx-1] = -num
-            num -= 1
-            idx += 1  
-
+        # while num > 0:
+        #     ans[idx] = num
+        #     ans[n-idx-1] = -num
+        #     num -= 1
+        #     idx += 1  
+        
+        for i in range(-(n//2), n//2+1, 1):
+            if i==0 and n%2==0:
+                continue;   
+            ans[idx] = i
+            idx+=1
+            
         return ans
         
