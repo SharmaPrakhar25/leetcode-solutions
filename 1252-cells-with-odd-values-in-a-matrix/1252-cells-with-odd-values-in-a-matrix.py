fe6ad8a86fa6affa9aab8e3ace4cf8ba5c51ba36
Prakhar Sharma <prakhar.sharma2512@gmail.com>
2022-01-12 09:09:02 +0530

    Time: 100 ms (10.79%), Space: 14.3 MB (76.80%) - LeetHub

--- 1252-cells-with-odd-values-in-a-matrix/1252-cells-with-odd-values-in-a-matrix.py
+++ 1252-cells-with-odd-values-in-a-matrix/1252-cells-with-odd-values-in-a-matrix.py
@@ -8,18 +8,23 @@ class Solution:
             
             while col < n:
                 ansmatrix[idx[0]][col] += 1
-                if ansmatrix[idx[0]][col]%2 != 0:
-                    totalodd += 1
-                else:
-                    totalodd -= 1
+                # if ansmatrix[idx[0]][col]%2 != 0:
+                #     totalodd += 1
+                # else:
+                #     totalodd -= 1
                 col += 1
             
             while row < m:
                 ansmatrix[row][idx[1]] += 1
-                if ansmatrix[row][idx[1]]%2 != 0:
-                    totalodd += 1
-                else:
-                    totalodd -= 1
+                # if ansmatrix[row][idx[1]]%2 != 0:
+                #     totalodd += 1
+                # else:
+                #     totalodd -= 1
                 row += 1
         
+        
+        for row in range(m):
+            for col in range(n):
+                if ansmatrix[row][col] % 2 == 1:
+                    totalodd += 1
         return totalodd
