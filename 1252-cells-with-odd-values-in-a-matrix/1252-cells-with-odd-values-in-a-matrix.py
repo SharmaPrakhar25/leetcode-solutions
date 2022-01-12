b3ec399714b2b39dfa2b0f6e283508b744e91ca7
Prakhar Sharma <prakhar.sharma2512@gmail.com>
2022-01-12 09:12:39 +0530

    Time: 68 ms (25.72%), Space: 14.3 MB (76.80%) - LeetHub

--- 1252-cells-with-odd-values-in-a-matrix/1252-cells-with-odd-values-in-a-matrix.py
+++ 1252-cells-with-odd-values-in-a-matrix/1252-cells-with-odd-values-in-a-matrix.py
@@ -3,28 +3,42 @@ class Solution:
         totalodd = 0
         ansmatrix = [[0 for col in range(n)]for row in range(m)]
         for idx in indices:
-            col = 0
-            row = 0
+            # col = 0
+            # row = 0
             
-            while col < n:
-                ansmatrix[idx[0]][col] += 1
-                # if ansmatrix[idx[0]][col]%2 != 0:
-                #     totalodd += 1
-                # else:
-                #     totalodd -= 1
-                col += 1
-            
-            while row < m:
-                ansmatrix[row][idx[1]] += 1
-                # if ansmatrix[row][idx[1]]%2 != 0:
-                #     totalodd += 1
-                # else:
-                #     totalodd -= 1
-                row += 1
-        
-        
-        for row in range(m):
             for col in range(n):
-                if ansmatrix[row][col] % 2 == 1:
+                ansmatrix[idx[0]][col] += 1
+                if ansmatrix[idx[0]][col]%2 != 0:
                     totalodd += 1
+                else:
+                    totalodd -= 1
+                    
+            # while col < n:
+            #     ansmatrix[idx[0]][col] += 1
+            #     if ansmatrix[idx[0]][col]%2 != 0:
+            #         totalodd += 1
+            #     else:
+            #         totalodd -= 1
+            #     col += 1
+            
+            for row in range(m):
+                ansmatrix[row][idx[1]] += 1
+                if ansmatrix[row][idx[1]]%2 != 0:
+                    totalodd += 1
+                else:
+                    totalodd -= 1
+                    
+            # while row < m:
+            #     ansmatrix[row][idx[1]] += 1
+            #     if ansmatrix[row][idx[1]]%2 != 0:
+            #         totalodd += 1
+            #     else:
+            #         totalodd -= 1
+            #     row += 1
+        
+        
+        # for row in range(m):
+        #     for col in range(n):
+        #         if ansmatrix[row][col] % 2 == 1:
+        #             totalodd += 1
         return totalodd
