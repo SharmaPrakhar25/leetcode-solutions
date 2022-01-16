ccd300dcb6d483425729b66cf432b5a74b243a75
Prakhar Sharma <prakhar.sharma2512@gmail.com>
2022-01-16 06:40:21 +0530

    Time: 179 ms (30.76%), Space: 15.9 MB (5.54%) - LeetHub

---
+++ 849-maximize-distance-to-closest-person/849-maximize-distance-to-closest-person.py
@@ -0,0 +1,25 @@
+class Solution:
+    def maxDistToClosest(self, seats: List[int]) -> int:
+        mapper = {}
+        lastFilledIndex = None
+        for idx in range(len(seats)):
+            if seats[idx] == 1:
+                lastFilledIndex = idx
+                continue
+            else:
+                if lastFilledIndex is not None:
+                    mapper[idx] = abs(lastFilledIndex - idx)
+                else:
+                    mapper[idx] = float(inf)
+                
+        
+        for idx in reversed(range(len(seats))):
+            if seats[idx] == 1:
+                lastFilledIndex = idx
+                continue
+            else:
+                mapper[idx] = min(mapper[idx],abs(lastFilledIndex - idx))
+            
+        return max(mapper.values())
+
+            
