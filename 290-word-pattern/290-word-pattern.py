8e3e43b1e26e50fab77e8973e00278e5f12780a8
Prakhar Sharma <prakhar.sharma2512@gmail.com>
2022-01-17 07:33:12 +0530

    Time: 56 ms (7.34%), Space: 14.3 MB (55.31%) - LeetHub

--- 290-word-pattern/290-word-pattern.py
+++ 290-word-pattern/290-word-pattern.py
@@ -2,21 +2,28 @@ class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
         mapper = {}
         s = s.split(' ')
-        temps = []
+        # temps = []
         if len(s) != len(pattern):
             return False
         
         for idx in range(len(s)):
-            if pattern[idx] not in mapper and s[idx] not in mapper.values():
-                mapper[pattern[idx]] = s[idx]
-                
-        for char in pattern:
-            if char not in mapper:
-                return False
+            if pattern[idx] not in mapper:
+                if s[idx] not in mapper.values():
+                    mapper[pattern[idx]] = s[idx]
+                else:
+                    return False
             else:
-                temps.append(mapper[char])
+                if mapper[pattern[idx]] != s[idx]:
+                    return False
+                
+        # for char in pattern:
+        #     if char not in mapper:
+        #         return False
+        #     else:
+        #         temps.append(mapper[char])
         
-        return True if temps == s else False
+        # print(mapper)
+        return True if len(set(pattern)) == len(mapper.keys()) else False
             
             
             
