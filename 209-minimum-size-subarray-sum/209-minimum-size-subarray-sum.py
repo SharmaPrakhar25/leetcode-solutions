05f2be7612429ca030657052f4ee421c0553d986
Prakhar Sharma <prakhar.sharma2512@gmail.com>
2022-02-03 20:21:28 +0530

    Time: 162 ms (10.19%), Space: 16.9 MB (13.03%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.py
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.py
@@ -4,13 +4,12 @@ class Solution:
         window_end = 0
         minLen = float('inf')
         tsum = 0
-        while window_end < len(nums) :
+        while window_end < len(nums):
             tsum += nums[window_end]
-            if tsum >= target:
-                while tsum >= target:
-                    minLen = min(minLen, window_end - window_start + 1)
-                    tsum -= nums[window_start]
-                    window_start += 1
+            while tsum >= target:
+                minLen = min(minLen, window_end - window_start + 1)
+                tsum -= nums[window_start]
+                window_start += 1
             window_end += 1
             
         return minLen if minLen != float('inf') else 0
