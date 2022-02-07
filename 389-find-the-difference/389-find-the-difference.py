7cdb07f98258321bfa44b649bad86e80c5c234fb
Prakhar Sharma <prakhar.sharma2512@gmail.com>
2022-02-07 08:32:44 +0530

    Time: 63 ms (19.07%), Space: 14.2 MB (50.98%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.py
+++ 389-find-the-difference/389-find-the-difference.py
@@ -1,15 +1,22 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        char_freq = {}
-        for char in s:
-            if char not in char_freq:
-                char_freq[char] = 0
-            char_freq[char] += 1
+        ans = 0
+        for i in range(len(s)):
+            ans = ans ^ ord(s[i]) ^ ord(t[i])
+            print(ans)
+        ans ^= ord(t[-1])
+        return chr(ans)
+#         char_freq = {}
+#         for char in s:
+#             if char not in char_freq:
+#                 char_freq[char] = 0
+#             char_freq[char] += 1
     
-        for char in t:
-            if char in char_freq and char_freq[char]:
-                char_freq[char] -= 1
-            else:
-                return char
+#         for char in t:
+#             if char in char_freq and char_freq[char]:
+#                 char_freq[char] -= 1
+#             else:
+#                 return char
+
                 
         
