cbcef547183267bb01d1cf4203ac05c575024300
Prakhar Sharma <prakhar.sharma2512@gmail.com>
2022-02-15 21:26:56 +0530

    Time: 220 ms (62.03%), Space: 43.4 MB (5.78%) - LeetHub

--- 344-reverse-string/344-reverse-string.py
+++ 344-reverse-string/344-reverse-string.py
@@ -1,12 +1,16 @@
 class Solution:
-	def reverseString(self, s: List[str]) -> None:
-		"""
-		Do not return anything, modify s in-place instead.
-		"""
-		def revStr(st, start, end):
-			if start >= end:
-				return
-			st[start], st[end] = st[end], st[start]
-			revStr(st, start+1, end-1)
-
-		revStr(s, 0, len(s)-1)
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        if len(s) == 1:
+            return s
+        return self.reverseChar(s, 0, len(s)-1)
+    
+    def reverseChar(self,s, start, end):
+        if end > start:
+            s[start], s[end] = s[end], s[start]
+            self.reverseChar(s, start+1, end-1)
+        else:
+            return s
+        
