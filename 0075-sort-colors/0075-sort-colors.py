7534b6eee5d5f919d3ccf569dd203fc5bd2dd013
Prakhar Sharma <prakhar.sharma2512@gmail.com>
2024-07-19 11:19:03 +0530

    Time: 34 ms (78.59%), Space: 16.4 MB (57.54%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,32 +3,17 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        """
-        2,0,2,1,1,0 
-        zero_index = we will do a for loop and get 0th index for 0 number 
-        last_index = we will do another for loop and 5th index for 2 number 
+        temp = [0] * 3
+        for num in nums:
+            temp[num] += 1
         
-        one_index = we will start from 1st index and one_index < last_index
-        if one_index == 1:
-            one_index += 1
+        i = 0
+        for index in range(len(temp)):
+            curr = index
+            count = temp[index]
+            while count > 0:
+                nums[i] = curr
+                i+= 1
+                count -= 1
+        return nums
         
-        if one_index == 0:
-            replace the number at zero_index and one_index += 1 and zero_index += 1
-        
-        if one_index == 2:
-            replace the number at last_index and last_index -= 1
-        """
-        red, white = 0, 0 
-        blue = len(nums) - 1
-        while white <= blue:
-            if nums[white] == 0:
-                nums[white], nums[red] = nums[red], nums[white]
-                white += 1
-                red += 1
-            elif nums[white] == 1:
-                white += 1
-            else:
-                nums[white], nums[blue] = nums[blue], nums[white]
-                blue -= 1  
-        return  
-                
